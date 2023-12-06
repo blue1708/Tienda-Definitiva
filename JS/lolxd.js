39dc9c5083176279ba9a3c7ad0f259b3068577d1
blue1708 <147755787+blue1708@users.noreply.github.com>
2023-12-06 01:51:22 -0600

    Add files via upload

---
+++ JS/lolxd.js
@@ -0,0 +1,9 @@
+let btn_lol = document.querySelector('#btn_lol')
+btn_lol.addEventListener('click', function (){
+    alert('Formulario Guardado')
+})
+
+let btn_xd = document.querySelector('#btn_xd')
+btn_xd.addEventListener('click', function(){
+    alert('Formulario Eliminado')
+})
